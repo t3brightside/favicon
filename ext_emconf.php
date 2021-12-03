56f8f08e6d5b68df4dd8378ba2c21c60f658151a
Tanel Põld <tanel@brightside.ee>
2021-12-03 07:57:02 +0200

    [TASK] Code clean up

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,18 @@
 <?php
-	$EM_CONF[$_EXTKEY] = array (
-		'title' => 'Favicon',
-		'description' => 'Add favicon through page settings. Renders icons for different devices.',
-		'category' => 'fe',
-		'version' => '0.1.2',
-		'state' => 'stable',
-		'uploadfolder' => false,
-		'createDirs' => '',
-		'clearcacheonload' => true,
-		'author' => 'Tanel Põld',
-		'author_email' => 'tanel@brightside.ee',
-		'author_company' => 'Brightside OÜ / t3brightside.com',
-		'constraints' =>
-			array (
-				'depends' =>
-				array (
-					'typo3' => '11.5.0 - 11.5.99',
-				),
-				'conflicts' =>
-				array (
-				),
-				'suggests' =>
-				array (
-				),
-			),
-		);
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Favicon',
+    'description' => 'Add favicon through page settings. Renders icons for different devices.',
+    'category' => 'fe',
+    'version' => '0.1.0',
+    'state' => 'stable',
+    'clearcacheonload' => true,
+    'author' => 'Tanel Põld',
+    'author_email' => 'tanel@brightside.ee',
+    'author_company' => 'Brightside OÜ / t3brightside.com',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0 - 11.5.99',
+        ],
+    ],
+];
